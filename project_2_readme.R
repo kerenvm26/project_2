59bc69c16c8a8880f84e0954acb6ccfb296a1838
kerenvm26 <134894786+kerenvm26@users.noreply.github.com>
2023-10-12 00:19:38 -0400

    Delete project_2_readme.R

--- project_2_readme.R
+++
@@ -1,7 +0,0 @@
-
-library(rmarkdown) #To load package
-
-# Render the R Markdown file to README.md
-rmarkdown::render("project_2.Rmd", output_format = "github_document", output_file = "README.md")
-
-
